55bb83d09d4ab2b974135d83e9a7b436962aeb84
vishal <vishal.t@borqs.com>
2024-06-27 18:59:05 +0530

    Added permissioon denied check

--- Camera/Camera22/app/src/main/java/com/example/camera2/MainActivity.kt
+++ Camera/Camera22/app/src/main/java/com/example/camera2/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.camera2
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.camera2.ui.theme.Camera2Theme
@@ -25,7 +45,7 @@ private val requestPermissionLauncher = registerForActivityResult(ActivityResult
         }
     }
     else{
-        TODO("If permissions not granted")
+        showPermissionDenied();
     }
 }
 
@@ -48,7 +68,6 @@ private val requestPermissionLauncher = registerForActivityResult(ActivityResult
         super.onDestroy();
         cameraHelper.stopProcessingFrames();
         cameraHelper.closeCamera();
-
     }
 
 
@@ -64,6 +83,40 @@ private fun hasPermissionRequired(): Boolean{
     }
 }
 
-
+private fun showPermissionDenied(){
+    setContent {
+        Camera2Theme {
+            Surface(modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background)) {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.DarkGray),
+                    contentAlignment = Alignment.Center){
+                    Column (modifier = Modifier.padding(20.dp)){
+                        Text(
+                            text = "Camera permissions are needed\nPlease enable permissions in settings to use this app",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Button(onClick = {
+                            goToSettings()
+                        }) {
+                            Text(text = "Go to settings")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
+private fun goToSettings(){
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply{
+            data = Uri.fromParts("package", packageName,null);
+        }
+    startActivity(intent);
+    }
+
+
+}
